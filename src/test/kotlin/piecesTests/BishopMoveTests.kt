864e381fd3a47643cd9907c41963f3bb46ebf6f1
André Santos <andre.david.santos.02@gmail.com>
2021-11-09 16:51:57 +0000

    Added Knight and Bishop MoveTests.

--- src/test/kotlin/piecesTests/BishopMoveTests.kt
+++ src/test/kotlin/piecesTests/BishopMoveTests.kt
@@ -5,16 +5,47 @@ import Move
 import getBoardFromString
 import kotlin.test.*
 
+// Bishop in e2
 private const val testBoard =
             "        " +
             "        " +
             "        " +
             "        " +
             "        " +
-            "        " +
-            "        " +
+            "     p  " +
+            "    B   " +
             "        "
 
 class BishopMoveTests {
     private val board = Board(getBoardFromString(testBoard))
+
+    @Test
+    fun `Bishop vertical not valid`() {
+        assertFalse(board.checkMove(Move("Be2e3")))
+    }
+
+    @Test
+    fun `Bishop horizontal not valid`() {
+        assertFalse(board.checkMove(Move("Be2d2")))
+    }
+
+    @Test
+    fun `Bishop move(up,right) with capture is valid`() {
+        assertTrue(board.checkMove(Move("Be2f3")))
+    }
+
+    @Test
+    fun `Bishop move(up,left) is valid`() {
+        assertTrue(board.checkMove(Move("Be2d3")))
+    }
+
+    @Test
+    fun `Bishop move(down,right) is valid`() {
+        assertTrue(board.checkMove(Move("Be2f1")))
+    }
+
+    @Test
+    fun `Bishop move(down,left) is valid`() {
+        assertTrue(board.checkMove(Move("Be2d1")))
+    }
 }
--- src/test/kotlin/piecesTests/KnightMoveTests.kt
+++ src/test/kotlin/piecesTests/KnightMoveTests.kt
@@ -5,16 +5,62 @@ import Move
 import getBoardFromString
 import kotlin.test.*
 
+// Knight in c3
 private const val testBoard =
             "        " +
             "        " +
             "        " +
+            "   p    " +
             "        " +
-            "        " +
-            "        " +
+            "  N     " +
             "        " +
             "        "
 
 class KnightMoveTests {
     private val board = Board(getBoardFromString(testBoard))
+
+    @Test
+    fun `Knight upper right with capture is valid`() {
+        assertTrue(board.checkMove(Move("Nc3d5")))
+    }
+
+    @Test
+    fun `Knight upper left is valid`() {
+        assertTrue(board.checkMove(Move("Nc3b5")))
+    }
+
+    @Test
+    fun `Knight down right is valid`() {
+        assertTrue(board.checkMove(Move("Nc3d1")))
+    }
+
+    @Test
+    fun `Knight down left is valid`() {
+        assertTrue(board.checkMove(Move("Nc3b1")))
+    }
+
+    @Test
+    fun `Knight right up is valid`() {
+        assertTrue(board.checkMove(Move("Nc3e4")))
+    }
+
+    @Test
+    fun `Knight right down is valid`() {
+        assertTrue(board.checkMove(Move("Nc3e2")))
+    }
+
+    @Test
+    fun `Knight left up is valid`() {
+        assertTrue(board.checkMove(Move("Nc3a4")))
+    }
+
+    @Test
+    fun `Knight left down is valid`() {
+        assertTrue(board.checkMove(Move("Nc3a2")))
+    }
+
+    @Test
+    fun `Knight not L shaped move is not valid`() {
+        assertFalse(board.checkMove(Move("Nc3e3")))
+    }
 }
